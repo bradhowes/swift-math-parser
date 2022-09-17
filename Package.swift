9ba5a4cfc966c03e2a11fe3fa353817e02f0cda6
Brad Howes <bradhowes@mac.com>
2022-09-17 10:13:27 +0200

    Cleanup

--- Package.swift
+++ Package.swift
@@ -1,25 +1,13 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "swift-math-parser",
-    // Duplicated swift-parsing values
-    platforms: [
-      .iOS(.v13),
-      .macOS(.v10_15),
-      .tvOS(.v13),
-      .watchOS(.v6),
-    ],
-    products: [
-        .library(
-            name: "MathParser",
-            targets: ["MathParser"]),
-    ],
-    dependencies: [
-      .package(url: "https://github.com/pointfreeco/swift-parsing", from: "0.10.0"),
-    ],
+    platforms: [.iOS(.v13), .macOS(.v10_15), .tvOS(.v13), .watchOS(.v6)],
+    products: [.library(name: "MathParser", targets: ["MathParser"])],
+    dependencies: [.package(url: "https://github.com/pointfreeco/swift-parsing", from: "0.10.0")],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
