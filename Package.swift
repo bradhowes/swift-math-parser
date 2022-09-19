560d67a1a5d8df99bcd3997a759ee9a4722b67c8
Brad Howes <bradhowes@mac.com>
2022-09-19 06:45:00 +0200

    Trying to builds

--- Package.swift
+++ Package.swift
@@ -1,21 +1,38 @@
 // swift-tools-version:5.5
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: "swift-math-parser",
-    platforms: [.iOS(.v13), .macOS(.v10_15), .tvOS(.v13), .watchOS(.v6)],
-    products: [.library(name: "MathParser", targets: ["MathParser"])],
-    dependencies: [.package(url: "https://github.com/pointfreeco/swift-parsing", from: "0.10.0")],
-    targets: [
-        .target(
-            name: "MathParser",
-            dependencies: [.product(name: "Parsing", package: "swift-parsing")]),
-        .testTarget(
-            name: "MathParserTests",
-            dependencies: ["MathParser", .product(name: "Parsing", package: "swift-parsing")]),
-    ]
+  name: "swift-math-parser",
+  platforms: [
+    .iOS(.v13),
+    .macOS(.v10_15),
+    .tvOS(.v13),
+    .watchOS(.v6),
+  ],
+  products: [
+    .library(
+      name: "MathParser",
+      targets: ["MathParser"]
+    )
+  ],
+  dependencies: [
+    .package(url: "https://github.com/pointfreeco/swift-parsing", from: "0.10.0")
+  ],
+  targets: [
+    .target(
+      name: "MathParser",
+      dependencies: [
+        .product(name: "Parsing", package: "swift-parsing"),
+      ]
+    ),
+    .testTarget(
+      name: "MathParserTests",
+      dependencies: [
+        "MathParser",
+      ]
+    )
+  ]
 )
 
 #if swift(>=5.6)
