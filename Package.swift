e9fc124b5d03665e21462a01e47a61091e0c3de8
Brad Howes <bradhowes@mac.com>
2022-09-19 06:23:46 +0200

    Cleanup.

--- Package.swift
+++ Package.swift
@@ -9,8 +9,6 @@ let package = Package(
     products: [.library(name: "MathParser", targets: ["MathParser"])],
     dependencies: [.package(url: "https://github.com/pointfreeco/swift-parsing", from: "0.10.0")],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "MathParser",
             dependencies: [.product(name: "Parsing", package: "swift-parsing")]),
