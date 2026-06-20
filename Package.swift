f9f10ec592217269d94496ee08bc7309ef22d257
Brad Howes <bradhowes@mac.com>
2026-06-20 22:04:08 +0200

    Bump swift-parsing version.

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
   ],
   dependencies: [
     .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
-    .package(url: "https://github.com/pointfreeco/swift-parsing", from: "0.12.0")
+    .package(url: "https://github.com/pointfreeco/swift-parsing", from: "0.15.0")
   ],
   targets: [
     .target(
