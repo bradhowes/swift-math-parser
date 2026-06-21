dfe7f1bc41564975bfee8a4bd0af162c26c2eac4
Brad Howes <bradhowes@mac.com>
2026-06-21 10:22:10 +0200

    Try to fix Linux build

---
+++ Package@swift-6.1.swift
@@ -0,0 +1,38 @@
+// swift-tools-version: 6.1
+
+import PackageDescription
+
+let package = Package(
+  name: "swift-math-parser",
+  platforms: [
+    .iOS(.v13),
+    .macOS(.v10_15),
+    .tvOS(.v13),
+    .watchOS(.v6),
+  ],
+  products: [
+    .library(
+      name: "MathParser",
+      targets: ["MathParser"]
+    )
+  ],
+  dependencies: [
+    .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
+    .package(url: "https://github.com/pointfreeco/swift-parsing", from: "0.15.0")
+  ],
+  targets: [
+    .target(
+      name: "MathParser",
+      dependencies: [.product(name: "Parsing", package: "swift-parsing")],
+      swiftSettings: [
+        .enableUpcomingFeature("InternalImportsByDefault"),
+        .enableUpcomingFeature("MemberImportVisibility")
+      ]
+    ),
+    .testTarget(
+      name: "MathParserTests",
+      dependencies: [.product(name: "Parsing", package: "swift-parsing"), "MathParser"]
+    )
+  ],
+  swiftLanguageModes: [.v6, .v5]
+)
